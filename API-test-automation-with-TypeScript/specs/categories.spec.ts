d5390cb17d767834ab2c883b39385e83a97784ef
RenanBa <renanbarbosadesouza@gmail.com>
2026-01-07 20:58:06 -0800

    Add delete and update test

--- API-test-automation-with-TypeScript/specs/categories.spec.ts
+++ API-test-automation-with-TypeScript/specs/categories.spec.ts
@@ -21,6 +21,11 @@ describe('Categories tests', () => {
     })
 
     describe('Create category', () => {
+        let categoryToDelete: any
+        afterAll(async () => {
+            await categoryCtrl.deleteCategoryById(categoryToDelete._id, token)
+        })
+
         it ('POST /categories - success', async () => {
             const name = "Test Category " + Math.floor(Math.random() * 100000)
             const playload = {
@@ -28,6 +33,58 @@ describe('Categories tests', () => {
             }
             const res = await categoryCtrl.postCategory(playload, token)
             console.log(res.body)
+            expect(res.status).toBe(200)
+            categoryToDelete = res.body
+        })
+    })
+
+    describe('Update category', () => {
+        let newCategory: any
+        beforeAll(async () => {
+            const name = "Test Category " + Math.floor(Math.random() * 100000)
+            const playload = {
+                'name': name
+            }
+            const newCat = await categoryCtrl.postCategory(playload, token)
+            newCategory = newCat.body
+        })
+
+        afterAll(async () => {
+            await categoryCtrl.deleteCategoryById(newCategory._id, token)
+        })
+
+        it ('PUT /categories/:id - success', async () => {
+            const playload = {
+                'name': 'New category name' + Math.floor(Math.random() * 100000)
+            }
+            const updatedCategory = await categoryCtrl.putCategoryById(newCategory._id, playload, token)
+            console.log(updatedCategory.body)
+            expect (updatedCategory.status).toBe(200)
+            expect (updatedCategory.body.name).toBe(playload.name)
+        })
+    })
+
+    describe('Delete category', () => {
+        let categoryToDelete: any
+        beforeAll(async () => {
+            const name = "Test Category " + Math.floor(Math.random() * 100000)
+            const playload = {
+                'name': name
+            }
+            const newCat = await categoryCtrl.postCategory(playload, token)
+            categoryToDelete = newCat.body
+        })
+
+        afterAll(async () => {
+            try {
+                await categoryCtrl.deleteCategoryById(categoryToDelete._id, token)
+            } catch (error) {
+                console.log(`Category already deleted: ${categoryToDelete._id}`)
+            }
+        })
+
+        it ('DELETE /categories/:id - success', async () => {
+            const res = await categoryCtrl.deleteCategoryById(categoryToDelete._id, token)
             expect(res.status).toBe(200)
         })
     })
